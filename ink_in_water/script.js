8ff5aa63fc3878240cbab0840d6c6774a6680e2e
Zebbeni <andrew.albers@gmail.com>
2017-12-03 14:14:22 -0700

    add bleed effect

--- ink_in_water/script.js
+++ ink_in_water/script.js
@@ -5,16 +5,14 @@ var ctx = canvas.getContext('2d');
 var raf;
 
 window.onresize = function () {
-  canvas.width = document.body.clientWidth; //document.width is obsolete
-  canvas.height = document.body.clientHeight; //document.height is obsolete
-  initializeForces();
+  initialize();
 }
 
 window.onload = function() {
   initialize();
 }
 
-var DROPLETS_PER_SPOT = 200;
+var DROPLETS_PER_SPOT = 100;
 var FORCE_SPACING = 20;
 var MIN_FORCE = 0.2;
 var MAX_FORCE = 0.8;
@@ -25,6 +23,10 @@ var droplets = [];
 function initialize() {
   canvas.width = document.body.clientWidth; //document.width is obsolete
   canvas.height = document.body.clientHeight; //document.height is obsolete
+  ctx.beginPath();
+  ctx.fillStyle = "rgba(0, 0, 0, 255)";
+  ctx.fillRect(0,0, canvas.width, canvas.height);
+  ctx.closePath();
   initializeForces();
 }
 
@@ -46,9 +48,35 @@ class Force {
     this.vx = vx;
     this.vy = vy;
   }
-  // update() {
+}
 
-  // }
+class Bleeder {
+  constructor(x, y, radius, r, g, b, a) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.r = r;
+    this.g = g;
+    this.b = b;
+    this.a = a;
+  }
+  update() {
+    this.radius++;
+    this.a -= 0.05;
+  }
+  draw() {
+    ctx.beginPath();
+    var colorInside = "rgba(" + this.r + "," + this.g + "," + this.b + "," + this.a + ")";
+    var colorOutside = "rgba(" + this.r + "," + this.g + "," + this.b + "," + 0.0 + ")";
+    var gradient = ctx.createRadialGradient(this.x, this.y, this.radius, this.x, this.y, 0);
+    gradient.addColorStop(0, colorOutside);
+    gradient.addColorStop(1, colorInside);
+    ctx.fillStyle = gradient;
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
+    ctx.fill();
+    ctx.closePath();
+    this.update();
+  }
 }
 
 class Droplet {
@@ -62,6 +90,7 @@ class Droplet {
     this.g = g;
     this.b = b;
     this.a = a;
+    this.bleeders = [];
   }
   update() {
     this.a *= 0.98;
@@ -78,17 +107,29 @@ class Droplet {
     }
     this.vx = this.vx * 0.9 + sumVx;
     this.vy = this.vy * 0.9 + sumVy;
-    var max_speed = this.radius * 0.5;
-    var speed = Math.sqrt(Math.pow(this.vx, 2) + Math.pow(this.vy, 2));
-    if (speed > max_speed) {
-      this.vx *= max_speed / speed;
-      this.vy *= max_speed / speed;
-    }
+    var speed = this.radius * 0.5;
+    var magnitude = Math.sqrt(Math.pow(this.vx, 2) + Math.pow(this.vy, 2));
+    // if (speed > max_speed) {
+      this.vx *= speed / magnitude;
+      this.vy *= speed / magnitude;
+    // }
+    // if (this.a > 0.5) {
+      this.bleeders.push(new Bleeder(this.x, this.y, this.radius, this.r, this.g, this.b, this.a));
+    // }
   }
   draw() {
+    var numBleeders = this.bleeders.length;
+    for (var b = 0; b < numBleeders; b++) {
+      if (this.bleeders[b].a < 0.1) {
+        this.bleeders.splice(b, 1);
+        b--;
+        numBleeders--;
+      } else {
+        this.bleeders[b].draw();
+      }
+    }
     ctx.beginPath();
     var colorInside = "rgba(" + this.r + "," + this.g + "," + this.b + "," + this.a + ")";
-    // var colorInside = "rgba(" + 255 + "," + 255 + "," + 255 + "," + this.a + ")";
     var colorOutside = "rgba(" + this.r + "," + this.g + "," + this.b + "," + 0.0 + ")";
     var gradient = ctx.createRadialGradient(this.x, this.y, this.radius, this.x, this.y, 0);
     gradient.addColorStop(0, colorOutside);
@@ -103,7 +144,7 @@ class Droplet {
 
 function draw() {
   // ctx.beginPath();
-  // ctx.fillStyle = "rgba(255, 255, 255, 0.009)";
+  // ctx.fillStyle = "rgba(0, 0, 0, 0.004)";
   // ctx.fillRect(0,0, canvas.width, canvas.height);
   // ctx.closePath();
 
@@ -128,14 +169,15 @@ canvas.addEventListener('mouseup', function(e) {
   var g = Math.round(Math.random() * 255);
   var b = Math.round(Math.random() * 255);
   var size = 0.5 + Math.random() * 1.0;
+  var mainAngle = Math.random() * 2.0 * Math.PI;
   for (var d = 0; d < DROPLETS_PER_SPOT; d++) {
-    var angle = Math.random() * 2.0 * Math.PI;
+    var angle = mainAngle - 0.5 + Math.random() * 0.25;
     var magnitude = Math.random() * size;
     var vx = Math.cos(angle) * magnitude;
     var vy = Math.sin(angle) * magnitude;
     var x = e.x + vx;
     var y = e.y + vy;
-    droplets.push(new Droplet(x, y, vx * 5.0, vy * 5.0, Math.random() * 10.0 * size, 0, 0, 0, 1.0));
+    droplets.push(new Droplet(x, y, vx * 10.0, vy * 10.0, Math.random() * 10.0 * size, r, g, b, 1.0));
   }
   raf = window.requestAnimationFrame(draw);
 });
